1a0202508e5003601fb9ae4d036157c2cf69961f
lelukeonfly@gmail.com <lelukeonfly@gmail.com>
2021-12-22 19:53:27 +0100

    split html head off page

--- index.php
+++ index.php
@@ -1,33 +1,13 @@
 <?php
     require_once 'model/funktionen.inc.php';
-    require_once 'model/database.inc.php';
     
 
     $aktion = isset($_REQUEST['aktion'])?$_REQUEST['aktion']:'dashboard';
     
+    echo "Aktion: ".$aktion;
     // LOGIK
     switch($aktion) {
         case 'dashboard':
-            $adressen = getAdressen();
-            break;
-    	case "zeige":
-        	$eintrag = hole_eintrag($_REQUEST['id']);
-    		break;
-    	case "neu":
-            $eintrag = leerer_eintrag();
-        	$aktion = 'formular_anzeigen';
-    		break;
-    	case "speichere":
-            speichere($_POST);
-    		break;
-    	case "editiere":
-        	$eintrag = hole_eintrag($_REQUEST['id']);
-        	$aktion = 'formular_anzeigen';
-    		break;	
-        case "loeschen":
-            $eintrag = loeschen($_REQUEST['id']);
-            $adressen = getAdressen();
-            $aktion = 'zeige_alle';
             break;
     }
     
