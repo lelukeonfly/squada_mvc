a706482d6e34cbdcf6ebf4861e7a74cad4cbd0f0
lelukeonlfy@gmail.com <lelukeonlfy@gmail.com>
2022-01-05 22:17:56 +0100

    add: quick styling for funciton testing

--- testing_functions.php
+++ testing_functions.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            display: flex;
+            flex-wrap: wrap;
+            gap: 5em;
+        }
+    </style>
+</head>
+<body>
+
 <?php
 include 'model/funktionen.inc.php';
 
@@ -6,152 +23,169 @@ function nl()
 echo "<br />";
 }
 
-function nl2()
-{
-echo "<br />";
-echo "<br />";
-}
 
+echo "<div>";
 echo "get_db_connection";
 nl();
 echo "<textarea>";
 var_dump(get_db_connection());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
 #miss: SETADMIN()
 
+echo "<div>";
 echo "get_players";
 nl();
 echo "<textarea>";
 var_dump(get_players());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "get_player";
 nl();
 echo "<textarea>";
 var_dump(get_player(3));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
 #miss: log_in()
 
+echo "<div>";
 echo "getPlayerImage";
 nl();
 echo "<textarea>";
 var_dump(getPlayerImage(get_player(3)['name']));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "getTeamImage";
 nl();
 echo "<textarea>";
 var_dump(getTeamImage((get_player(3)['mannschaft'])));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "is_logged_in";
 nl();
 echo "<textarea>";
 var_dump(is_logged_in());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
 #miss: register
 
+echo "<div>";
 echo "getUsername";
 nl();
 echo "<textarea>";
 var_dump(getUsername(1,true));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "getTeuerstenPlayer";
 nl();
 echo "<textarea>";
 var_dump(getTeuerstenPlayer());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "seeOfferedPlayers";
 nl();
 echo "<textarea>";
 var_dump(seeOfferedPlayers(1));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "comparePassword";
 nl();
 echo "<textarea>";
 var_dump(comparePassword("test","test"));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
 #miss: editPassword
 #miss: changeUsersettings 
 #miss: setGuthaben
 #miss: bieten 
 
+echo "<div>";
 echo "navbar";
 nl();
 echo "<textarea>";
 var_dump(navbar());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "head";
 nl();
 echo "<textarea>";
 var_dump(head());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "footer";
 nl();
 echo "<textarea>";
 var_dump(footer());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "loginResult";
 nl();
 echo "<textarea>";
 var_dump(loginResult(false));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "logout";
 nl();
 echo "<textarea>";
 var_dump(logout());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "getLogoURL";
 nl();
 echo "<textarea>";
 var_dump(getLogoURL());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "success";
 nl();
 echo "<textarea>";
 var_dump(success());
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "getTimestampWhenSpielerNichtMehrUnterVertragIst";
 nl();
 echo "<textarea>";
 var_dump(getTimestampWhenSpielerNichtMehrUnterVertragIst(3));
 echo "</textarea>";
-nl2();
+echo "</div>";
 
+echo "<div>";
 echo "getPlayersNotInVertrag";
 nl();
 echo "<textarea>";
 var_dump(getPlayersNotInVertrag());
 echo "</textarea>";
-nl2();
+echo "</div>";
+?> 
+</body>
+</html>
