32bb065d85b59a96f63d4cf808a6207fbc6469e9
Vozodo <raphaelbarbieri638@gmail.com>
2022-01-10 20:24:39 +0100

    fix: view date

--- model/funktionen.inc.php
+++ model/funktionen.inc.php
@@ -642,3 +642,10 @@ function isAuktion($spieler)
     $return = $statement->fetch();
     
 }
+
+//Format seconds to nice view
+function secondsToTime($seconds) {
+    $dtF = new \DateTime('@0');
+    $dtT = new \DateTime("@$seconds");
+    return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
+}
